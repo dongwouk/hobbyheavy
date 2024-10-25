795fe7d026a28bf8541a54016002f7e8e4252515
kwon <kwondw30@gmail.com>
2024-10-25 14:23:44 +0900

    .

---
+++ src/main/java/com/example/hobbyheavy/HobbyheavyApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbyheavy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyheavyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyheavyApplication.class, args);
+    }
+
+}
