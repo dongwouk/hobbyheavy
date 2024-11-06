3c1ab9810088b927a701ddd92f993ab8042d0cdb
soohomode <soohomode@gmail.com>
2024-11-06 17:31:26 +0900

    Refactor: 코드리뷰 수정 제안 이행

--- src/main/java/com/example/hobbyheavy/entity/Hobby.java
+++ src/main/java/com/example/hobbyheavy/entity/Hobby.java
@@ -18,13 +18,13 @@ public class Hobby extends Base {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long hobbyId;
 
-    @Column(name = "hobby_name", unique = true, nullable = false, length = 50)
+    @Column(name = "hobby_name", nullable = false)
     private String hobbyName; // 취미명
 
     @Column(name = "in_out_door", nullable = false)
     private String inOutDoor; // 실/내외
 
-    @Column(length = 50)
+    @Column
     private String category; // 카테고리
 
 }
