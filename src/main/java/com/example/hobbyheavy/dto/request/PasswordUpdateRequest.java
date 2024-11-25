2acd9b9c2648b68df6df5bc1c2138b9f629f5754
soohomode <soohomode@gmail.com>
2024-11-25 17:46:58 +0900

    Refactor: 리팩토링

--- src/main/java/com/example/hobbyheavy/dto/request/PasswordUpdateRequest.java
+++ src/main/java/com/example/hobbyheavy/dto/request/PasswordUpdateRequest.java
@@ -1,7 +1,5 @@
 package com.example.hobbyheavy.dto.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
